2dca0aa51b61da844c7fc9e1e82a63341b749f62
Leandro Casanova <casanova.leandro@gmail.com>
2018-11-07 19:34:59 -0200

    Salvando uuid do dispositivo

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
 alunos = [
     {
         'id': 1,
+        'uuid':'',
         'ra': u'20606884',
         'nome': u'EVANDRO DOS SANTOS', 
         'cpf': u'37160902807'
     },
     {
         'id': 2,
+        'uuid':'',
         'ra': u'20547540',
         'nome': u'RUTE CARDOSO', 
         'cpf': u'37160902806'
     },
     {
         'id': 3,
+        'uuid':'',
         'ra': u'20904866',
         'nome': u'RAFAEL OKADA', 
         'cpf': u'37160902805'
@@ -49,6 +52,17 @@ def get_aluno_nome(aluno_nome):
     if len(aluno) == 0:
         abort(404)
     return jsonify({'aluno': aluno[0]})
+
+@app.route('/alunos/cadastro/<aluno_ra>', methods=['PUT'])
+def update_aluno(aluno_ra):
+    aluno = [aluno for aluno in alunos if aluno['ra'] == aluno_ra]
+    if len(aluno) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+
+    aluno[0]['uuid'] = request.json.get('uuid', aluno[0]['uuid'])
+    return jsonify({'aluno': aluno[0]}), 201
 
 
 @app.errorhandler(404)
